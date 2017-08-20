039888ccb9ee4786d94884b2bc87e64fe1af373f
pathfindermind <artem.tretyakov.91@inbox.ru>
2017-08-20 23:57:55 +0300

    Мутатор one to one

--- blog/app/Http/Controllers/Admin/Core.php
+++ blog/app/Http/Controllers/Admin/Core.php
@@ -26,87 +26,12 @@ class Core extends Controller
 
     public function getArticles(Request $request)
     {
-       //$articles = DB::table('articles')->get();
-        //$articles = DB::table('articles')->pluck('name');
-        //$articles = DB::table('articles')->count();
-        //$articles = DB::table('articles')->max('id');
-        /*$articles = DB::table('articles')->chunk(2, function($articles){
-                foreach ($articles as $article) {
-                    Core::addArticles($article);
-                }
-        });*/
-        //$query = DB::table('articles')->select('name');/*
-        //$query = DB::table('articles');
-         //$articles = $query->addSelect('text')->get();*/
-
-        /*$articles = $query->select('text')
-                                ->where('id', '=', 5)->get()
-                                ->where('name', 'like', 'test%')->get();*/
-
-       /* $articles = $query->select('text')
-                                ->where([
-                                    ['id','>',5],
-                                    ['name', 'like', 'test%', 'or']
-                                ])
-            ->get();
-
-       // dump(self::$articles);
-       dump($articles);*/
-
-       /*$articles = Article::where('id', '>', 3)->orderby('name')->take(2)->get();
-
-       foreach ($articles as $article) {
-           echo $article->name.'</br>';
-       }*/
-
-
-       /*Article::find([1,2,3]);
-       Article::findOrFail();*/
-       /* $article = new Article;
-        $article->name = 'New Article';
-        $article->text = 'New Text';
-
-        $article->save();*/
-
-       $articles = Article::all();
-        //$articles = Article::withTrashed()->get();
-        //$articles = Article::withTrashed()->restore();
-
-        $article = Article::find(2);
-        //$article->forceDelete();
-        //$article->delete();
-
-        //dump($articles);
-
-        $user = User::find(1);
-
-        $role = $user->roles()->where('roles.id',2)->first();
-
         $country = Country::find(1);
+        $user = User::find(2);
 
-        //$article = $user->articles->where('id', 2)->first();
+        $country->user()->associate($user);
 
-       /* foreach ($articles as $article) {
-            echo $article->name.'<br>';
-        }*/
-
-        //dump($article->user->name);
-
-        //dump($user->roles);
-
-        //$articles = Article::with('user')->get();
-        $articles->load('user');
-
-        foreach ($articles as $article) {
-           dump($article->user->name).'<br>';
-       }
-
-
-
-
-        dump($articles);
-
-
+        $country->save();
 
     }
 
